a6ce6f86a5c7fdca7b015e65d10437071da71ea0
dezlymacauley <dezlymacauley@proton.me>
2026-07-08 16:21:12 +0200

    Added dependencies for connecting to Postgres

---
+++ rust-lab/04-external-dependencies/ed-05-sqlx/src/bin/01_connecting_to_postgres.rs
@@ -0,0 +1,15 @@
+/*
+    ABOUT: Connecting to Postgres
+
+    Install the following dependencies:
+    
+    cargo add sqlx --features postgres,runtime-tokio
+
+    cargo add tokio --features full
+
+    cargo add dotenvy
+*/
+
+fn main() {
+
+}
