fed9df3500f16a55661c805716679ab29338803e
dezlymacauley <dezlymacauley@proton.me>
2026-07-08 19:22:09 +0200

    Added a simple check if the .env file exists

--- rust-lab/04-external-dependencies/ed-05-sqlx/src/bin/01_connecting_to_postgres.rs
+++ rust-lab/04-external-dependencies/ed-05-sqlx/src/bin/01_connecting_to_postgres.rs
@@ -1,5 +1,7 @@
 /*
     ABOUT: Connecting to Postgres
+    
+    ___________________________________________________________________________
 
     Install the following dependencies:
     
@@ -8,8 +10,29 @@
     cargo add tokio --features full
 
     cargo add dotenvy
+    
+    ___________________________________________________________________________
+    
+    Create a `.env` file at the root of this project
+
+    Use the file `.env.example` as a guide for what should be in the `.env`
+    file
+
+    ___________________________________________________________________________
 */
 
+use std::path::PathBuf;
+
 fn main() {
+
+    // This is how you get the absolute file path in Rust
+    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    // This is how you get the absolute file path of the `.env` file
+    let dot_env_file_path: PathBuf = project_root.join(".env");
+
+    println!("Does .env file exist? {}", dot_env_file_path.exists());
+
+    // assert!();
 
 }
