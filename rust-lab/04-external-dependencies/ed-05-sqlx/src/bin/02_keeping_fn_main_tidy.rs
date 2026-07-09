03a9ee25442a1881149169a1430f87a797980ad7
dezlymacauley <dezlymacauley@proton.me>
2026-07-09 10:12:33 +0200

    The load_dot_env function is now functional

--- rust-lab/04-external-dependencies/ed-05-sqlx/src/bin/02_keeping_fn_main_tidy.rs
+++ rust-lab/04-external-dependencies/ed-05-sqlx/src/bin/02_keeping_fn_main_tidy.rs
@@ -1,20 +1,23 @@
 /*
-    ABOUT: Keeping `fn main` clean
+    ABOUT: Keeping `fn main` tidy
 
     ___________________________________________________________________________
+
+    The first way to keep `fn main` tidy is to group the logic of the program
+    into function that perform a specific task.
 
     ___________________________________________________________________________
 */
 
 // Standard Library Imports
-use std::path::PathBuf;
 use std::env;
+use std::path::PathBuf;
 
-// External Dependencies
-use dotenvy::Error;
-
-fn load_dot_env() -> Result<(), Error> {
-
+// This function will attempt to load the variables listed in the `.env` file
+// 1. If `load_dot_env` succeeds it will return an empty tuple (aka a unit)
+// to the caller.
+// 2. If the function fails, it will return `dotenvy::Error`.
+fn load_dot_env() -> Result<(), dotenvy::Error> {
     // This is the absolute path to the Cargo.toml file of the project.
     // The Cargo.toml file is always at the root of a Rust project.
     let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -23,49 +26,41 @@ fn load_dot_env() -> Result<(), Error> {
     let dot_env_file_path: PathBuf = project_root.join(".env");
 
     // In Rust, the last expression of a function is automatically returned.
-    // `from_path` returns a Result enum.
-    // Result <(), Error> 
     dotenvy::from_path(dot_env_file_path)
 }
 
 fn main() {
-
     //_________________________________________________________________________
 
     // STEP: 1 => Load the variables listed in the `.env` file into the
     // process environment of the program
 
+    if let Err(error_message) = load_dot_env() {
+        eprintln!("\n❌ Failed to load `.env` variables: {error_message}\n");
 
-    // let dotenvy_status: Result<(), Error> =
-    //     dotenvy::from_path(dot_env_file_path);
-    //
-    // match dotenvy_status {
-    //     Ok(()) => {
-    //         println!("\nSuccessfully loaded `.env` variables");
-    //     }
-    //     Err(error_message) => {
-    //         println!("\nFailed to load `.env` variables: {error_message}\n");
-    //         // Exit the program after showing the error message above.
-    //         return;
-    //     }
-    // }
+        // Always remember to add the `return` statement to make the program
+        // exit after printing the message to stderr.
+        return;
+    }
+
+    println!("\n✅ Successfully loaded `.env` variables");
 
     //_________________________________________________________________________
 
     // STEP: 2 => Use `env::var()` save environment variables from
     // the process environment into Rust variables.
 
-    let database_name =
-        env::var("DATABASE_NAME").expect("DATABASE_NAME is not set");
-
-    let host = env::var("HOST").expect("HOST is not set");
-
-    let port = env::var("PORT").expect("PORT is not set");
-
-    let user_name = env::var("USER_NAME").expect("USER_NAME is not set");
-
-    let user_password =
-        env::var("USER_PASSWORD").expect("USER_PASSWORD is not set");
+    // let database_name =
+    //     env::var("DATABASE_NAME").expect("DATABASE_NAME is not set");
+    //
+    // let host = env::var("HOST").expect("HOST is not set");
+    //
+    // let port = env::var("PORT").expect("PORT is not set");
+    //
+    // let user_name = env::var("USER_NAME").expect("USER_NAME is not set");
+    //
+    // let user_password =
+    //     env::var("USER_PASSWORD").expect("USER_PASSWORD is not set");
 
     //_________________________________________________________________________
 
@@ -79,19 +74,14 @@ fn main() {
     // It is not a file path, so you don't need to worry about the `//`,
     // because the URI is platform independent.
 
-    let connection_string: String = format!(
-        // database_type://username:password@host:port/database_name
-        "postgres://{user_name}:{user_password}@{host}:{port}/{database_name}"
-    );
-
-    println!("\nconnection_string: {connection_string}\n");
+    // let connection_string: String = format!(
+    //     // database_type://username:password@host:port/database_name
+    //     "postgres://{user_name}:{user_password}@{host}:{port}/{database_name}"
+    // );
+    //
+    // println!("\nconnection_string: {connection_string}\n");
 
     //_________________________________________________________________________
-    
-
-
-
-
 
     //_________________________________________________________________________
 }
