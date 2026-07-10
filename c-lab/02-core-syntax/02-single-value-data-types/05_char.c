f31cb2bf9255e6d8dc1cac3d7bc5a19f60b8e0e5
dezlymacauley <dezlymacauley@proton.me>
2026-07-10 10:14:18 +0200

    Covered the majority of single value data types in C

--- c-lab/02-core-syntax/02-single-value-data-types/05_char.c
+++ c-lab/02-core-syntax/02-single-value-data-types/05_char.c
@@ -1,10 +1,14 @@
 /*
     ABOUT: char
 
-    signed char
-    unsigned char
 */
 
+#include <stdio.h>
 int main() {
-    
+
+    // The value must be wrapped in single qoutes
+    char first_letter = 'A';
+
+    printf("first_letter: %c\n", first_letter);
+    // first_letter: A
 }
