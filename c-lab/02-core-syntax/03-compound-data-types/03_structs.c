3f8f27ae115ea830d2a87363be5364f8a68d5ce2
dezlymacauley <dezlymacauley@proton.me>
2026-07-11 08:30:42 +0200

    Updated knowldege on C structs

--- c-lab/02-core-syntax/03-compound-data-types/03_structs.c
+++ c-lab/02-core-syntax/03-compound-data-types/03_structs.c
@@ -1,21 +1,71 @@
 /*
     ABOUT: Structs
+
 */
 
 #include <stdio.h>
 
+// A struct is declared outside a function
 struct Person {
-    int age;
-    float height;
+    int age; // 4 bytes (32 bits)
+    float height; // 4 bytes (32 bits)
 };
 
 int main(void) {
 
-    struct Person alice = {
+    //_________________________________________________________________________
+    
+    // EXAMPLE: 1 => Printing the size of a struct in bytes
+
+    printf("The size of person_one is %zu bytes\n", sizeof(struct Person));
+    // The size of person_one is 8 bytes
+
+    //_________________________________________________________________________
+    
+    // EXAMPLE: 2 => Creating an instance of a struct
+
+    struct Person person_one = {
         .age = 25,
         .height = 1.72f,
     };
 
-    printf("alice.age = %i\n", alice.age);
-    printf("alice.height = %.2f\n", alice.height);
+    printf("person_one.age = %i\n", person_one.age);
+    printf("person_one.height = %.2f\n", person_one.height);
+
+    //_________________________________________________________________________
+
+    // EXAMPLE: 3 => Creating a pointer to a struct.
+
+    // The variable memory_address_of_person_one stores the memory address
+    // of where the variable person_one is stored in memory.
+
+    // A variable that stores the memory address of another variable
+    // is called a pointer.
+    //
+    // The data type of memory_address_of_person_one,
+    // is `struct Person*`. This means "a pointer to a Person struct"
+    // `&person_one` is how you tell C that you want to store
+    // the memory address of person_one, and not its value.
+    struct Person* memory_address_of_person_one = &person_one;
+
+    //_________________________________________________________________________
+
+    // EXAMPLE: 4 => Printing the memory address of a struct.
+
+    printf(
+        "memory_address_of_person_one: %p\n",
+        (void*)memory_address_of_person_one
+    );
+    // memory_address_of_person_one: 0x7ffdb7d8ecc8
+
+    //_________________________________________________________________________
+
+    // EXAMPLE: 5 => Printing the value of a struct using a pointer
+
+    printf("person_one.age: %i\n", memory_address_of_person_one->age);
+    printf("person_one.height: %.2f\n", memory_address_of_person_one->height);
+    // person_one.age: 25
+    // person_one.height: 1.72
+
+    //_________________________________________________________________________
 }
