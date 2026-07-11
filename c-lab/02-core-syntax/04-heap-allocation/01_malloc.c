2ee96b63d1668d85a1a58ef0857a7b660b80001a
dezlymacauley <dezlymacauley@proton.me>
2026-07-11 10:33:00 +0200

    Starting revision on malloc

---
+++ c-lab/02-core-syntax/04-heap-allocation/01_malloc.c
@@ -0,0 +1,18 @@
+/*
+    ABOUT: Singly Linked List - Creating the first Node in the list
+
+    ___________________________________________________________________________
+
+*/
+
+struct Node {
+    int data;
+    struct Node* next_node;
+};
+
+int main() {
+
+    struct Node* start_of_list = nullptr;
+
+    return 0;
+}
