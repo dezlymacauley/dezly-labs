51065620ce781915aa32a63c76750aaca9b84570
dezlymacauley <dezlymacauley@proton.me>
2026-07-11 12:20:19 +0200

    Added last Node

---
+++ c-lab/02-core-syntax/06-data-structures-and-algorithms/02_singly_linked_list_the_start_and_end_of_list_markers.c
@@ -0,0 +1,43 @@
+/*
+    ABOUT: Singly Linked List - The `start` and `end` of list markers
+
+    The first thing that needs to be created for a Linked List is the start
+    of list marker. 
+
+    This list stores the memory address of the first Node in the list,
+    or it is set to `nullptr` if the list is empty.
+
+    `nullptr` is short for null pointer. 
+    It's a way of expressing that when the list is empty,
+    the variable start_of_list does not contain any valid memory address.
+
+    ___________________________________________________________________________
+   
+    Then you do the same thing for the end_of_list_marker.
+
+    ___________________________________________________________________________
+    
+    The reason why you need these markers,
+    is to keep track of where the list starts and where the list ends as
+    Nodes are added and deleted from the list.
+
+    E.g. If you add three Nodes and then delete the first Node,
+    then the order of the list changes and `start_of_list` has to be updated
+    to store the memory address of the new first Node.
+
+    ___________________________________________________________________________
+
+*/
+
+struct Node {
+    int data;
+    struct Node* next_node;
+};
+
+int main() {
+
+    struct Node* start_of_list = nullptr;
+    struct Node* end_of_list = nullptr;
+
+    return 0;
+}
