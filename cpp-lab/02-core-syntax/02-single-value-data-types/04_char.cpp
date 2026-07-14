932e1e6bd2c674c5aa7335760bc04cc3597f5cd7
dezlymacauley <dezlymacauley@proton.me>
2026-07-14 14:59:44 +0200

    Added char and string type

---
+++ cpp-lab/02-core-syntax/02-single-value-data-types/04_char.cpp
@@ -0,0 +1,21 @@
+/*
+ 
+    ABOUT: char
+
+    This is for storing a single character.
+*/
+
+#include <print>
+
+using std::println;
+
+int main() {
+
+    // The value must be wrapped in single qoutes.
+    char current_letter = 'D';
+
+    println("current_letter: {}", current_letter);
+    // current_letter: D
+
+    return 0;
+}
---
+++ cpp-lab/02-core-syntax/03-compound-data-types/01_string.cpp
@@ -0,0 +1,24 @@
+/*
+
+    ABOUT: string
+
+*/
+
+#include <print>
+using std::println;
+
+#include <string>
+using std::string;
+
+int main() {
+
+    string userName = "dezlymacauley";
+    string displayName = "Dezly Macauley";
+
+    println("userName: {}", userName);
+    println("displayName: {}", displayName);
+    // userName: dezlymacauley
+    // displayName: Dezly Macauley
+
+    return 0;
+}
