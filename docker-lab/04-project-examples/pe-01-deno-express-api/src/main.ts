e5c67ed5890836afed15e757dfcffd62932cc73a
dezlymacauley <dezlymacauley@proton.me>
2026-07-15 08:09:28 +0200

    Added post and get routes

--- docker-lab/04-project-examples/pe-01-deno-express-api/src/main.ts
+++ docker-lab/04-project-examples/pe-01-deno-express-api/src/main.ts
@@ -4,10 +4,37 @@ import type { Express } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+const users: string[] = [];
+
 app.get(
   "/",
   (_req, res) => {
     res.send("Hello world");
+  },
+);
+
+app.get(
+  "/users",
+  (_req, res) => {
+    return res.json({ users: users });
+  },
+);
+
+app.post(
+  "/users",
+  (req, res) => {
+    const newUserId = req.body.userId;
+
+    if (!newUserId) {
+      return res.status(400).send("Missing userId");
+    }
+
+    if (users.includes(newUserId)) {
+      return res.status(400).send("userId already exists");
+    }
+
+    users.push(newUserId);
+    return res.status(201).send("User registered");
   },
 );
 
