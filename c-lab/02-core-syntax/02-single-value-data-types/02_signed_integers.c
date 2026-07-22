b1e7f6ee0211e3c931dda73ef8ed5eaab9ab4263
dezlymacauley <dezlymacauley@proton.me>
2026-07-22 19:17:06 +0200

    Updated notes on integer types in C

--- c-lab/02-core-syntax/02-single-value-data-types/02_signed_integers.c
+++ c-lab/02-core-syntax/02-single-value-data-types/02_signed_integers.c
@@ -7,16 +7,44 @@
     long long
 */
 
-
-
 // integers are numbers that don't have a fractional part.
 
-int main() {
+#include <stdio.h>
 
-  // The correct format specifier is %d which 
+int main() {
+  // The correct format specifier is %d which
   // means `decimal integer` (base 10)
 
+  // `%o` mean `octal integer` (base 8)
+  // `%x` mean `hexadecimal integer` (also refered to as `hex`) (base 16)
+
+  // and `%i` is just a general format specifier for an integer.
+  // When you use this with `scanf()`,
+  // C will try to work out whether the integer is
+  // base 10, base 8, or base 16 based on its value.
+
+  int user_age = 32;
+
+  // 0 followed by the three numbers
+  // The value printed will only show 775,
+  // the 0 at the start will be left out.
+  int file_permissions = 0775;
+
+  // `70008B` is purple color
+  // In frontend development, more specifically CSS,
+  // you'd use `#` to indicate that it is a hex value like this:
+  // color: #70008B;
   //
+  // In C, an integer value can't start with `#`,
+  // so you'd use `0x` to indicate that it is a hex value like this:
+  // The value printed will only show 70008B,
+  // the 0x at the start will be left out.
+  int color = 0x70008B;
 
-
+  printf("user_age: %d\n", user_age);
+  printf("file_permissions: %o\n", file_permissions);
+  printf("color: %x\n", color);
+  // user_age: 32
+  // file_permissions: 775
+  // color: 70008b
 }
